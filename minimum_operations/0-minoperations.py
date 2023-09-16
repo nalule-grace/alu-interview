499b12c85c586c1db4702e578d0a1f1b3a56396d
nalule-grace <g.nalule@alustudent.com>
2023-09-16 13:59:59 -0700

    pep

--- minimum_operations/0-minoperations.py
+++ minimum_operations/0-minoperations.py
@@ -13,27 +13,21 @@ def minOperations(n):
     counter = 0
 
     while chars_pasted < n:
-#while above and if nothing in copied
         if copied == 0:
-            #copy
             copied = chars_pasted
             counter += 1
 
-        #while above and if nothing pasted
         if chars_pasted == 1:
-            #paste
             chars_pasted += copied
             counter += 1
             continue
 
         remaining = n - chars_pasted
         if remaining % chars_pasted != 0:
-            #paste current clipboard
             chars_pasted += copied
             counter += 1
 
         else:
-            #copy all
             copied = chars_pasted
             chars_pasted += copied
             counter += 2
