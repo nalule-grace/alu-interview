f24b8dd3355e5ca4b704555f6c7f1c14166ebd8b
nalule-grace <g.nalule@alustudent.com>
2023-10-05 03:58:34 -0700

    rain3

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -4,50 +4,44 @@
 Given a list of non-negative integers representing the heights of walls
 with unit width 1, as if viewing the cross-section of a relief map,
 calculate how many square units of water will be retained after it rains.
+
+Prototype: def rain(walls)
+walls is a list of non-negative integers.
+Return: Integer indicating total amount of rainwater retained.
+Assume that the ends of the list (before index 0 and after index walls[-1]) are
+not walls, meaning they will not retain water.
+If the list is empty return 0.
+
 """
-def calculate_rainwater(walls):
+
+
+def rain(walls):
     """
-    Calculate the total amount of rainwater that can be retained between walls.
+    Calculate how many square units of water will be retained after it rains.
 
     :param walls: A list of non-negative integers representing wall heights.
-    :return: An integer indicating the total amount of retained rainwater.
+    :return: Integer indicating the total amount of rainwater retained.
     """
 
-    # Check if there are any walls; if not, no water can be retained.
     if not walls:
         return 0
 
-    # Initialize pointers for the leftmost and rightmost positions.
     left, right = 0, len(walls) - 1
-
-    # Initialize variables to track the maximum heights from left and right.
     left_max, right_max = 0, 0
+    water = 0
 
-    # Initialize a variable to keep track of the total retained rainwater.
-    retained_water = 0
-
-    # Continue until the left pointer is less than the right pointer.
     while left < right:
-        # If the left wall is shorter or equal to the right wall,
         if walls[left] <= walls[right]:
-            # Update the left maximum height if needed.
             if walls[left] >= left_max:
                 left_max = walls[left]
-            # Calculate the water trapped at the left position and add it to retained_water.
             else:
-                retained_water += left_max - walls[left]
-            # Move the left pointer to the right.
+                water += left_max - walls[left]
             left += 1
-        else:  # If the right wall is taller than the left wall,
-            # Update the right maximum height if needed.
+        else:
             if walls[right] >= right_max:
                 right_max = walls[right]
-            # Calculate the water trapped at the right position and add it to retained_water.
             else:
-                retained_water += right_max - walls[right]
-            # Move the right pointer to the left.
+                water += right_max - walls[right]
             right -= 1
 
-    # Return the total retained rainwater.
-    return retained_water
-
+    return water
